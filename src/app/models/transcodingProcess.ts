1c13f24bc8137cd03d2d4c22f979482db144a27b
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-07-12 15:09:31 -0500

    JS - Enhancements

---
+++ src/app/models/transcodingProcess.ts
@@ -0,0 +1,13 @@
+
+export class TranscodingProcess{
+
+  public Id: number;
+  public RequestGuid: string;
+  public SystemProcessIdentifier: number;
+  public AdditionalSystemProcessIdentifier: number;
+  public GeneratedTemporaryFolder: string;
+  public MediaServerId: number;
+  public VideoId: number;
+  public PendingDeletion: boolean;
+
+}
