1fa605780f1118b96bff979b2bdeede74ad9a0a8
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-07-13 11:42:36 -0500

    JS - Enhancements

--- src/assets/sql/script.sql
+++ src/assets/sql/script.sql
@@ -2,6 +2,7 @@ INSERT INTO Catalog(Id,Name) VALUES (1,'Media Server State');
 INSERT INTO Catalog(Id,Name) VALUES (2,'Media Type');
 INSERT INTO Catalog(Id,Name) VALUES (3,'Video Transmision Type');
 INSERT INTO Catalog(Id,Name) VALUES (4,'Transcoder Preset');
+INSERT INTO Catalog(Id,Name) VALUES (5,'Tipo de Icono');
 
 INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (1,1,'Started');
 INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (1,2,'Stopped');
@@ -22,3 +23,9 @@ INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (4,6,'medium');
 INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (4,7,'slow');
 INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (4,8,'slower');
 INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (4,9,'veryslow');
+
+INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (5,1,'Font awesome');
+INSERT INTO CatalogDetail(CatalogId,Id,Name) VALUES (5,2,'Imagen');
+
+
+/*Despues de hacer las migraciones agregamos un usuario por defecto con el rol de administrador y un perfil*/
