f81e1e0a1bf9bf1d508bbbb2de11f8e7aa217505
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-07-20 22:42:32 -0500

    JS - Enhancements

--- src/models/storage.ts
+++ src/models/storage.ts
@@ -4,6 +4,7 @@ import { MediaType } from "../app/enums";
 export class Storage{
 
   public Id?: number;
+  public Name?:string;
   public MediaServerId?:number;
   public PhysicalPath: string;
   public MediaTypeCatalogId: number;
