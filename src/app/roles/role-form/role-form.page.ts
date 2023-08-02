33610dc6f27e31cb6a551b6d9fbba57d113ce706
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-02 17:46:40 -0500

    JS - Enhancements in role

--- src/app/roles/role-form/role-form.page.ts
+++ src/app/roles/role-form/role-form.page.ts
@@ -4,11 +4,10 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { SharedDataService } from 'src/app/shared/services/shared-data.service';
 import { HttpClient} from '@angular/common/http';
 import { RestProviderService } from 'src/app/shared/services/rest-provider.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ToastService, ToastType } from 'src/app/shared/services/toast.service';
 import { ParentComponent } from 'src/app/parent/parent.component';
-import { HttpMethodType } from 'src/app/shared/enums';
 import { Role } from '../shared/role.model';
 import { RoleService } from '../shared/role.service';
 import { Subscription } from 'rxjs';
@@ -24,6 +23,8 @@ export class RoleFormPage extends ParentComponent implements OnInit,OnDestroy {
   @ViewChild('modalTarget') modalTarget: ElementRef;
   roleForm:FormGroup;
   showModalSubscription$:Subscription;
+  modalRef:NgbModalRef;
+  showLoading:boolean=false;
 
   constructor(
     private ref:ChangeDetectorRef,
@@ -55,30 +56,34 @@ export class RoleFormPage extends ParentComponent implements OnInit,OnDestroy {
         enabled:new FormControl(this.role.Enabled)
       });
       this.ref.detectChanges();
-      this.modalService.open(this.modalTarget, {container:"#sinovadMainContainer",
+      this.modalRef= this.modalService.open(this.modalTarget, {container:"#sinovadMainContainer",
       modalDialogClass:'modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-dialog-scrollable',
-      scrollable:true,backdrop:'static'}).result.then((result) => {
-        this.saveItem();
-      }, (reason) => {
-        this.role=undefined;
-      });
+      scrollable:true,backdrop:'static'});
     }
 
     public saveItem(){
       if(this.roleForm.valid)
       {
+        this.showLoading=true;
         var role:Role=JSON.parse(JSON.stringify(this.role));
         role.Name=this.roleForm.value.name;
         role.Enabled=this.roleForm.value.enabled;
         this.roleService.saveItem(role).then((response) => {
+          this.showLoading=false;
           this.role=undefined;
           this.toastService.showToast({message:"Se guardo el rol satisfactoriamente",toastType:ToastType.Success});
+          this.modalRef.close();
         },error=>{
+          this.showLoading=false;
           this.toastService.showToast({message:error,toastType:ToastType.Error});
         });
       }else{
         this.roleForm.markAllAsTouched();
       }
+    }
+
+    public closeModal(){
+      this.modalRef.close();
     }
 
 }
