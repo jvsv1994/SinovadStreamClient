c73dc725d4334298206a6b172724f09b9cbc4e1e
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-02 11:26:14 -0500

    JS - Add custom list generic

--- src/app/roles/role-list/role-list.page.ts
+++ src/app/roles/role-list/role-list.page.ts
@@ -5,7 +5,6 @@ import { SharedDataService } from 'src/app/shared/services/shared-data.service';
 import { HttpClient} from '@angular/common/http';
 import { RestProviderService } from 'src/app/shared/services/rest-provider.service';
 import { SinovadApiPaginationResponse } from 'src/app/response/sinovadApiPaginationResponse';
-import { ParentComponent } from 'src/app/parent/parent.component';
 import { Role } from '../shared/role.model';
 import { RoleService } from '../shared/role.service';
 import { Subscription } from 'rxjs';
@@ -13,20 +12,15 @@ import { MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
 import { ToastService, ToastType } from 'src/app/shared/services/toast.service';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ConfirmDialogOptions, CustomConfirmDialogComponent } from 'src/app/shared/components/custom-confirm-dialog/custom-confirm-dialog.component';
+import { CustomListGeneric } from 'src/app/shared/generics/custom-list.generic';
 
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.page.html',
   styleUrls: ['./role-list.page.scss']
 })
-export class RoleListPage extends ParentComponent implements OnInit,OnDestroy {
+export class RoleListPage extends CustomListGeneric<Role>  implements OnInit,OnDestroy {
 
-  totalCount:number;
-  itemsPerPage:number=10;
-  currentPage:number=1;
-  listItems: Role[];
-  listSelectedItems:Role[]=[];
-  lastSelectedItem:Role;
   showContextMenu:boolean=false;
   refreshSubscription$:Subscription;
   showLoading:boolean=true;
@@ -41,8 +35,7 @@ export class RoleListPage extends ParentComponent implements OnInit,OnDestroy {
     public http: HttpClient,
     public domSanitizer: DomSanitizer,
     public sharedData: SharedDataService) {
-      super(restProvider,domSanitizer,sharedData)
-
+      super();
     }
 
     public ngOnInit(): void {
@@ -63,161 +56,6 @@ export class RoleListPage extends ParentComponent implements OnInit,OnDestroy {
 
     public ngOnDestroy(): void {
       this.refreshSubscription$.unsubscribe();
-    }
-
-    public isSelectedAll(){
-      if(this.listSelectedItems.length>0 && this.listSelectedItems.length==this.listItems.length)
-      {
-        return true;
-      }else{
-        return false;
-      }
-    }
-
-    public isSelectedAny(){
-      if(this.listSelectedItems.length>0 && this.listSelectedItems.length<this.listItems.length)
-      {
-        return true;
-      }else{
-        return false;
-      }
-    }
-
-    public isSelectedNothing(){
-      if(this.listSelectedItems.length==0)
-      {
-        return true;
-      }else{
-        return false;
-      }
-    }
-
-    public onChangeSelectAllCheckBox(event:any){
-      if(event.target.checked)
-      {
-        this.selectAll();
-      }else{
-        this.unselectAll();
-      }
-    }
-
-    public selectAll(){
-      for(let i=0;i < this.listItems.length;i++)
-      {
-        let item=this.listItems[i];
-        this.listSelectedItems.push(item);
-      }
-    }
-
-    public unselectAll(){
-      this.listSelectedItems=[];
-    }
-
-    public onSelectItem(event:any,item:Role)
-    {
-      let addItems:boolean=false;
-      let index=this.listSelectedItems.findIndex(ele=>ele.Id==item.Id);
-      if(index!=-1)
-      {
-        this.listSelectedItems.splice(index,1);
-        addItems=false;
-      }else{
-        this.listSelectedItems.push(item);
-        addItems=true;
-      }
-      if(event.shiftKey && this.lastSelectedItem)
-      {
-        let lastSelectedItemIndex=0
-        if(this.listSelectedItems.length>0 && this.lastSelectedItem)
-        {
-          lastSelectedItemIndex=this.listItems.indexOf(this.lastSelectedItem);
-        }
-        let currentIndex=this.listItems.indexOf(item);
-        if(addItems)
-        {
-          if(currentIndex>=lastSelectedItemIndex)
-          {
-            let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)<=currentIndex && this.listItems.indexOf(item)>=lastSelectedItemIndex && !this.isCheckedItem(item));
-            this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
-          }else{
-            let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)>=currentIndex && this.listItems.indexOf(item)<=lastSelectedItemIndex && !this.isCheckedItem(item));
-            this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
-          }
-        }else{
-          let listItemsToUnselect=[];
-          if(currentIndex>=lastSelectedItemIndex)
-          {
-            listItemsToUnselect=this.listItems.filter(item=>this.listItems.indexOf(item)<=currentIndex && this.listItems.indexOf(item)>=lastSelectedItemIndex && this.isCheckedItem(item));
-          }else{
-            listItemsToUnselect=this.listItems.filter(item=>this.listItems.indexOf(item)>=currentIndex && this.listItems.indexOf(item)<=lastSelectedItemIndex && this.isCheckedItem(item));
-          }
-          if(listItemsToUnselect && listItemsToUnselect.length>0)
-          {
-            listItemsToUnselect.forEach(element => {
-              let index=this.listSelectedItems.findIndex(item=>item.Id==element.Id);
-              if(index!=-1)
-              {
-                this.listSelectedItems.splice(index,1);
-              }
-            });
-          }
-        }
-      }
-      this.lastSelectedItem=item;
-    }
-
-    public isCheckedItem(item:Role){
-      let index=this.listSelectedItems.findIndex(ele=>ele.Id==item.Id);
-      if(index!=-1)
-      {
-        return true;
-      }else{
-        return false;
-      }
-    }
-
-    public onClickItem(event:any,item:Role)
-    {
-      if(event.ctrlKey || event.shiftKey)
-      {
-        if(event.ctrlKey)
-        {
-          if(this.listSelectedItems.indexOf(item)!=-1)
-          {
-            this.listSelectedItems.splice(this.listSelectedItems.indexOf(item),1)
-          }else{
-            this.listSelectedItems.push(item);
-          }
-        }else{
-          let lastSelectedItemIndex=0
-          if(this.listSelectedItems.length>0 && this.lastSelectedItem)
-          {
-            lastSelectedItemIndex=this.listItems.indexOf(this.lastSelectedItem);
-          }
-          let currentIndex=this.listItems.indexOf(item);
-          if(currentIndex>=lastSelectedItemIndex)
-          {
-            let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)<=currentIndex && this.listItems.indexOf(item)>=lastSelectedItemIndex && this.listSelectedItems.indexOf(item)==-1);
-            this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
-          }else{
-            let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)>=currentIndex && this.listItems.indexOf(item)<=lastSelectedItemIndex && this.listSelectedItems.indexOf(item)==-1);
-            this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
-          }
-        }
-      }else{
-        if(event.type=="contextmenu")
-        {
-          if(this.listSelectedItems.indexOf(item)==-1)
-          {
-            this.listSelectedItems=[];
-            this.listSelectedItems.push(item);
-          }
-        }else{
-          this.listSelectedItems=[];
-          this.listSelectedItems.push(item);
-        }
-      }
-      this.lastSelectedItem=item;
     }
 
     public showNewRol(){
---
+++ src/app/shared/generics/custom-list.generic.ts
@@ -0,0 +1,133 @@
+
+export class CustomListGeneric<T>{
+
+totalCount:number;
+itemsPerPage:number=10;
+currentPage:number=1;
+listItems:T[]=[];
+listSelectedItems:T[]=[];
+lastSelectedItem:T;
+
+constructor(
+) {
+
+}
+
+  public isSelectedAll(){
+
+    if(this.listSelectedItems.length>0 && this.listSelectedItems.length==this.listItems.length)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public isSelectedAny(){
+    if(this.listSelectedItems.length>0 && this.listSelectedItems.length<this.listItems.length)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public isSelectedNothing(){
+    if(this.listSelectedItems.length==0)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public onChangeSelectAllCheckBox(event:any){
+    if(event.target.checked)
+    {
+      this.selectAll();
+    }else{
+      this.unselectAll();
+    }
+  }
+
+  public selectAll(){
+    for(let i=0;i < this.listItems.length;i++)
+    {
+      let item=this.listItems[i];
+      this.listSelectedItems.push(item);
+    }
+  }
+
+  public unselectAll(){
+    this.listSelectedItems=[];
+  }
+
+  public onSelectItem(event:any,item:T)
+  {
+    let addItems:boolean=false;
+    let index=this.listSelectedItems.findIndex(ele=>ele["Id"]==item["Id"]);
+    if(index!=-1)
+    {
+      this.listSelectedItems.splice(index,1);
+      addItems=false;
+    }else{
+      this.listSelectedItems.push(item);
+      addItems=true;
+    }
+    if(event.shiftKey && this.lastSelectedItem)
+    {
+      let lastSelectedItemIndex=0
+      if(this.listSelectedItems.length>0 && this.lastSelectedItem)
+      {
+        lastSelectedItemIndex=this.listItems.indexOf(this.lastSelectedItem);
+      }
+      let currentIndex=this.listItems.indexOf(item);
+      if(addItems)
+      {
+        if(currentIndex>=lastSelectedItemIndex)
+        {
+          let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)<=currentIndex && this.listItems.indexOf(item)>=lastSelectedItemIndex && !this.isCheckedItem(item));
+          this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
+        }else{
+          let listItemsToAdd=this.listItems.filter(item=>this.listItems.indexOf(item)>=currentIndex && this.listItems.indexOf(item)<=lastSelectedItemIndex && !this.isCheckedItem(item));
+          this.listSelectedItems=this.listSelectedItems.concat(listItemsToAdd);
+        }
+      }else{
+        let listItemsToUnselect=[];
+        if(currentIndex>=lastSelectedItemIndex)
+        {
+          listItemsToUnselect=this.listItems.filter(item=>this.listItems.indexOf(item)<=currentIndex && this.listItems.indexOf(item)>=lastSelectedItemIndex && this.isCheckedItem(item));
+        }else{
+          listItemsToUnselect=this.listItems.filter(item=>this.listItems.indexOf(item)>=currentIndex && this.listItems.indexOf(item)<=lastSelectedItemIndex && this.isCheckedItem(item));
+        }
+        if(listItemsToUnselect && listItemsToUnselect.length>0)
+        {
+          listItemsToUnselect.forEach(element => {
+            let index=this.listSelectedItems.findIndex(item=>item["Id"]==element["Id"]);
+            if(index!=-1)
+            {
+              this.listSelectedItems.splice(index,1);
+            }
+          });
+        }
+      }
+    }
+    this.lastSelectedItem=item;
+  }
+
+  public isCheckedItem(item:T){
+    let index=this.listSelectedItems.findIndex(ele=>ele["Id"]==item["Id"]);
+    if(index!=-1)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public getNewIndex(itemIndex:number,pageIndex:number,pageSize:number){
+    var newIndex=(itemIndex+1)+((pageIndex-1)*pageSize);
+    return newIndex;
+  }
+
+}
