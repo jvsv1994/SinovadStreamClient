90ffe27e1fd1f20e42d6535dcb95b3253ceec502
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-01 20:40:23 -0500

    JS - Enhancements

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { ClickOutsideDirective } from 'src/app/shared/directives/click-outside-directive';
 import { FormatDataPipe } from './pipes/format-data.pipe';
 import { CustomConfirmDialogComponent } from './components/custom-confirm-dialog/custom-confirm-dialog.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule
   ],
   declarations: [
     FormatDataPipe,ClickOutsideDirective,CustomConfirmDialogComponent
