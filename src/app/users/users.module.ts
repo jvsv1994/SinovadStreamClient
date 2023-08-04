e9e70179c14944ea5b2af8a4bc6da25bc0708e49
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-04 14:50:53 -0500

    JS - Enhancements

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-import { UsersComponent } from './users.component';
 import { UserListPage } from './user-list/user-list.page';
 import { MediaServersModule } from '../media-servers/media-servers.module';
 
 @NgModule({
     declarations: [
-      UsersComponent,
       UserListPage
     ],
     providers:[],
