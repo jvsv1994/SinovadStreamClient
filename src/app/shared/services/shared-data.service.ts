5c1a4a77e954e94a4fa6b4a7961c00e2831d23a0
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-08-08 11:18:02 -0500

    JS - Enhancements

--- src/app/shared/services/shared-data.service.ts
+++ src/app/shared/services/shared-data.service.ts
@@ -5,6 +5,15 @@ import { Profile } from 'src/app/profiles/shared/profile.model';
 import { Configuration } from '../models/configuration.model';
 import { MediaServer } from 'src/app/servers/shared/server.model';
 import { FormGroup } from '@angular/forms';
+import { ItemDetail } from 'src/app/media/shared/item-detail.model';
+import { Item } from 'src/app/media/shared/item.model';
+import { RestProviderService } from './rest-provider.service';
+import {v4 as uuid} from "uuid";
+import { TranscodePrepareVideo } from 'src/app/media/video/models/transcodePrepareVideo';
+import { BuilderVideo } from 'src/app/media/video/models/builderVideo';
+import { Episode } from 'src/app/episodes/shared/episode.model';
+import { HttpMethodType } from '../enums';
+import { SinovadApiGenericResponse } from 'src/app/response/sinovadApiGenericResponse';
 
 @Injectable({ providedIn: 'root' })
 export class SharedDataService {
@@ -28,7 +37,7 @@ export class SharedDataService {
   mediaServers:MediaServer[]=[];
   selectedMediaServer:MediaServer;
 
-  constructor() {
+  constructor(public restProvider:RestProviderService) {
 
   }
 
@@ -81,5 +90,190 @@ export class SharedDataService {
     }
   }
 
+
+  public CreateBuilderVideoFromItem(item:Item,detail:ItemDetail):BuilderVideo{
+    let processGUID=uuid();
+    var transcodeVideo= new TranscodePrepareVideo();
+    transcodeVideo.VideoId=item.VideoId;
+    transcodeVideo.Title=item.Subtitle?item.Title+" "+item.Subtitle:item.Title;
+    transcodeVideo.PhysicalPath=item.PhysicalPath;
+    transcodeVideo.MediaServerId=item.MediaServerId;
+    transcodeVideo.MediaServerUrl=item.MediaServerUrl;
+    transcodeVideo.CurrentTime=item.CurrentTime;
+    transcodeVideo.ProcessGUID=processGUID;
+    if(transcodeVideo.CurrentTime)
+    {
+      transcodeVideo.TimeSpan=transcodeVideo.CurrentTime.toString();
+    }else{
+      transcodeVideo.TimeSpan="0";
+    }
+    if(detail.ListSeasons)
+    {
+      detail.CurrentSeason=detail.ListSeasons.find(season=>season.SeasonNumber==item.SeasonNumber);
+      detail.CurrentEpisode=detail.CurrentSeason.ListEpisodes.find(ep=>ep.EpisodeNumber==item.EpisodeNumber);
+    }
+    var builderVideo= new BuilderVideo();
+    builderVideo.TranscodePrepareVideo=transcodeVideo;
+    builderVideo.ItemDetail=detail;
+    return builderVideo;
+  }
+
+  public CreateBuilderVideoFromEpisode(episode:Episode,detail:ItemDetail):BuilderVideo{
+    var transcodeVideo:TranscodePrepareVideo=this.GetTranscodeVideoFromEpisode(episode);
+    if(detail.ListSeasons)
+    {
+      detail.CurrentSeason=detail.ListSeasons.find(item=>item.SeasonNumber==episode.SeasonNumber);
+      detail.CurrentEpisode=detail.CurrentSeason.ListEpisodes.find(item=>item.EpisodeNumber==episode.EpisodeNumber);
+    }
+    var builderVideo= new BuilderVideo();
+    builderVideo.TranscodePrepareVideo=transcodeVideo;
+    builderVideo.ItemDetail=detail;
+    return builderVideo;
+  }
+
+  public GetTranscodeVideoFromEpisode(episode:Episode):TranscodePrepareVideo{
+    let processGUID=uuid();
+    var transcodeVideo= new TranscodePrepareVideo();
+    transcodeVideo.VideoId=episode.VideoId;
+    transcodeVideo.Title=episode.TvSerieName+" "+"T"+episode.SeasonNumber+":E"+episode.EpisodeNumber+" "+episode.Name;
+    transcodeVideo.PhysicalPath=episode.PhysicalPath;
+    transcodeVideo.MediaServerId=episode.MediaServerId;
+    transcodeVideo.MediaServerUrl=episode.MediaServerUrl;
+    transcodeVideo.ProcessGUID=processGUID;
+    transcodeVideo.CurrentTime=0;
+    if(transcodeVideo.CurrentTime)
+    {
+      transcodeVideo.TimeSpan=transcodeVideo.CurrentTime.toString();
+    }else{
+      transcodeVideo.TimeSpan="0";
+    }
+    return transcodeVideo;
+  }
+
+  public getUser(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.restProvider.executeSinovadApiService(HttpMethodType.GET,'/users/GetUserData').then((response:SinovadApiGenericResponse) => {
+        let data=response.Data;
+        this.userData=data;
+        if(this.userData==null)
+        {
+          this.apiToken=undefined;
+          localStorage.removeItem("apiToken");
+        }else{
+          resolve(true);
+        }
+      },error=>{
+        console.error(error);
+        reject(error)
+      });
+    });
+  }
+
+
+  public getMediaServers(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.restProvider.executeSinovadApiService(HttpMethodType.GET,'/mediaServers/GetAllByUserAsync/'+this.userData.Id).then((response:SinovadApiGenericResponse) => {
+        let mediaServers=response.Data;
+        this.mediaServers=mediaServers;
+        this.checkSecureConnectionMediaServers();
+        resolve(true);
+      },error=>{
+        reject(error);
+      });
+    });
+  }
+
+  public checkSecureConnectionMediaServers(){
+    if(this.mediaServers!=null && this.mediaServers.length>0)
+    {
+      this.mediaServers.forEach(mediaServer => {
+        this.restProvider.executeHttpMethodByUrl(HttpMethodType.GET,mediaServer.Url+"/api").then((response) => {
+            mediaServer.isSecureConnection=true;
+            if(this.selectedMediaServer==undefined)
+            {
+              this.selectedMediaServer=this.mediaServers.find(ele=>ele.Id==mediaServer.Id);
+            }
+        },error=>{
+          mediaServer.isSecureConnection=false;
+        });
+      });
+    }
+  }
+
+  public getProfiles(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.restProvider.executeSinovadApiService(HttpMethodType.GET,'/profiles/GetAllWithPaginationByUserAsync/'+this.userData.Id).then((response:SinovadApiGenericResponse) => {
+        let listProfiles=response.Data;
+        this.listProfiles=listProfiles;
+        this.currentProfile=listProfiles[0];
+        resolve(true);
+      },error=>{
+        reject(error);
+      });
+    });
+  }
+
+  public getMenus(): Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.restProvider.executeSinovadApiService(HttpMethodType.GET,'/menus/GetByUserAsync/'+this.userData.Id).then((response:SinovadApiGenericResponse) => {
+        this.listMenus=response.Data;
+        resolve(true);
+      },error=>{
+        reject(error);
+      });
+    });
+  }
+
+  public isSmallDevice(){
+    if(window.innerWidth<=768)
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public isMobileDevice(){
+    if(('ontouchstart' in window))
+    {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  public getUrlByItemDetailMovieDataBase(item:ItemDetail){
+    if(item.TmdbId!=undefined && item.TmdbId>0)
+    {
+      return this.originalUrlImagesMovieDataBase+item.PosterPath;
+    }else{
+      return item.PosterPath;
+    }
+  }
+
+  public getUrlByItemMovieDataBase(item:Item){
+    if(item.TmdbId!=undefined && item.TmdbId>0)
+    {
+      return this.originalUrlImagesMovieDataBase+item.PosterPath;
+    }else{
+      return item.PosterPath;
+    }
+  }
+
+  public getYearByDateFormat(releaseDate:any){
+    var releaseDateConverted = new Date(releaseDate);
+    return releaseDateConverted.getFullYear();
+  }
+
+  public sort_by(field, reverse, primer) {
+    let key = primer ? (x) => primer(x[field]) : (x) => x[field];
+    reverse = !reverse ? 1 : -1;
+    return (a, b) => { return a = key(a), b = key(b), reverse * (+(a > b) - +(b > a)) }
+  }
+
+  public validateEmailFormat(email: string): boolean {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
 
 }
