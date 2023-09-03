856867dc211f201ff8b3267f189ca30ebc74db29
Jorge Victor Solis Vega <jvsv1994@gmail.com>
2023-09-03 09:54:55 -0500

    JS - Enhancements

--- src/app/components/splash-screen/splash-screen.component.ts
+++ src/app/components/splash-screen/splash-screen.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SharedDataService } from 'src/app/services/shared-data.service';
 
-import { RestProviderService } from 'src/app/modules/shared/services/rest-provider.service';
-
 declare var window;
 @Component({
   selector: 'app-splash-screen',
@@ -14,7 +12,6 @@ declare var window;
 export class SplashScreenComponent {
 
   constructor(
-    public restProvider: RestProviderService,
     public domSanitizer: DomSanitizer,
     public sharedDataService: SharedDataService) {
 
